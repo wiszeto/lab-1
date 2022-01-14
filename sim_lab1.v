f2b7f373284a66d70b76cc19275c203938605840
Wilson Szeto <85478086+wiszeto@users.noreply.github.com>
2022-01-13 17:12:20 -0800

    Update sim_lab1.v

--- sim_lab1.v
+++ sim_lab1.v
@@ -3,6 +3,23 @@
 // Company: 
 // Engineer: 
 // 
+// Create Date: 01/13/2022 12:44:53 AM
+// Design Name: 
+// Module Name: lab1sim
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
 // Create Date: 01/12/2022 03:04:21 PM
 // Design Name: 
 // Module Name: sim_lab1
@@ -26,12 +43,12 @@ module sim_lab1(
     reg clk;
     reg clr; //for first counter; this is a button
     reg BTN;
-    wire [3:0]an;
-    wire [6:0]seg;
+    wire [7:0] seg;
+    wire [3:0] an;
     
     
-    lab1_top lab(.clk(clk), .clr(clr), .BTN(BTN), .an(an), .seg(seg));
-    
+    Top lab(.clk(clk), .clr(clr), .BTN(BTN), .seg(seg), .an(an));
+
     
     initial
     begin
@@ -52,4 +69,13 @@ module sim_lab1(
     BTN = 0;
     
     end
+endmodule
+
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module lab1sim(
+
+    );
 endmodule
